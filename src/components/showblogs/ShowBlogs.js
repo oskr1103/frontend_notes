e79dba0fce87ca306409b535fd7808daf827660f
Isai Colina <isaicolinapinto@gmail.com>
2022-03-29 09:33:36 -0500

    add bar search

--- src/components/showblogs/ShowBlogs.js
+++ src/components/showblogs/ShowBlogs.js
@@ -8,14 +8,14 @@ const URI = "https://rehobot-notes.herokuapp.com/blogs/";
 
 const CompShowBlogs = () => {
   const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    getBlogs();
-  }, []);
+  const [blogsNotas, setBlogsNotas] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
   //Procedimiento para obtener los blogs
   const getBlogs = async () => {
     const result = await axios.get(URI);
-    setBlogs(result.data.sort((a, b) => b.id - a.id));
+    setBlogs(result.data);
+    setBlogsNotas(result.data);
     console.log(
       "App desarrollada con NodeJS, Express, ReactJS y MongoDB - Isai Colina"
     );
@@ -27,30 +27,68 @@ const CompShowBlogs = () => {
     getBlogs();
   };
 
+  const handlerChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const filtrar = (terminoBusqueda) => {
+    let resultado = blogsNotas.filter((elemento) => {
+      if (
+        elemento.title
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase()) ||
+        elemento.content
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase())
+      ) {
+        return elemento;
+      } else {
+        return null;
+      }
+    });
+    setBlogs(resultado);
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
   return (
     <div className="container__show">
+      <div className="input__search">
+        <input
+          value={busqueda}
+          placeholder="Buscar Nota"
+          onChange={handlerChange}
+        />
+      </div>
       <div className="grid-card">
-        {blogs.map((blog, index) => (
-          <div className="card__show" key={index}>
-            <div className="card-header">
-              <b>{blog.title}</b>
-            </div>
-            <div className="card-body">
-              <p className="card-text">{blog.content}</p>
-              <div className="btn-card">
-                <Link to={`/edit/${blog._id}`} className="btn-notesShow">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </Link>
-                <button
-                  onClick={() => deleteBlog(blog._id)}
-                  className="btn-notesShow"
-                >
-                  <i className="fa-solid fa-trash-can"></i>
-                </button>
+        {blogs &&
+          blogs.map((blog, index) => (
+            <div className="card__show" key={index}>
+              <div className="card-header">
+                <b>{blog.title}</b>
+              </div>
+              <div className="card-body">
+                <p className="card-text">{blog.content}</p>
+                <div className="btn-card">
+                  <Link to={`/edit/${blog._id}`} className="btn-notesShow">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Link>
+                  <button
+                    onClick={() => deleteBlog(blog._id)}
+                    className="btn-notesShow"
+                  >
+                    <i className="fa-solid fa-trash-can"></i>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
